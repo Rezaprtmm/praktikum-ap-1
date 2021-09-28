9aa5d7586a69be1a2d36510efeb889510e8e49ad
Rezaprtmm <90909246+Rezaprtmm@users.noreply.github.com>
2021-09-28 22:14:01 +0700

    Add files via upload

---
+++ tugas mencari kecepatan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float v,s,t;
+	cout<<"PROGRAM C++ MENGHITUNG KECEPATAN"<<endl;
+	cout<<endl;
+	cout<<"Jarak Tempuh (Km)  \t: ";
+	cin>>s;
+	cout<<"Waktu Tempuh (Jam) \t: ";
+	cin>>t;
+	v=s/t;
+	
+	cout<<"Kecepatan (Km/jam) \t: "<<v<<"Km/jam"<<endl;
+	
+	return 0;
+}
