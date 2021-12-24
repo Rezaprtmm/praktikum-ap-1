9fcc40d083f25f13b89121cf8978864865aac6f7
Rezaprtmm <90909246+Rezaprtmm@users.noreply.github.com>
2021-12-24 21:05:54 +0700

    Add files via upload

---
+++ file cpp ss Latihan Soal 2021/Latihan Soal No. 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int hitung() {
+	int kelereng = 100;
+	int bagi = 3;
+	int sisa;
+	return	sisa = kelereng % bagi;
+
+}
+int main() {
+	cout << "Sisa Kelereng adalah : " << hitung();
+}
